7b6d7679c8844f5400562cd37804da2456d9605b
christiancesar <christiancnp@gmail.com>
2021-05-09 00:03:19 -0400

    add favicon

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Favicon from 'react-favicon';
 import App from './App';
-
+import favicon from './assets/favicon.svg';
 ReactDOM.render(
   <React.StrictMode>
+    <Favicon url={favicon} />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
